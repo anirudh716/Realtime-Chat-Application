a1f44e6c93acf858d0d40e718bc1e81c3654807f
anirudh716 <anirudhpandey244@gmail.com>
2024-03-10 20:41:20 +0530

    made some changes

---
+++ server.js
@@ -0,0 +1,20 @@
+const io = require('socket.io')(8000);
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit("user-joined", name);
+    });
+    // when a user sends a message
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    });
+
+    //when a user disconnect
+    socket.on('disconnect', () =>{
+        const disconnectedUser = users[socket.id];
+        socket.broadcast.emit('user-left', disconnectedUser);
+        delete users[socket.id];
+    })
+});
